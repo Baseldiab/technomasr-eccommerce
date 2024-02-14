1263d9f26aeb0f033cabad3c0ba7ae27b8bc3af8
Basel Diab <114107867+Baseldiab@users.noreply.github.com>
2024-02-14 15:51:07 +0200

    update image of order

--- src/routes/cart/components/cart.order.jsx
+++ src/routes/cart/components/cart.order.jsx
@@ -1,12 +1,12 @@
 export default function CartOrder() {
   return (
-    <div className="mx-auto   h-[70vh] w-[50%] min-w-[250px] max-w-[600px] bg-[#F3F3F3] ">
+    <div className="mx-auto rounded-lg  h-[70vh] md:w-[50%] min-w-[250px] max-w-[600px] bg-[#F3F3F3] ">
       <div className="text mx-auto py-5">
         <h2 className="text-primary text-center font-bold">Your order is on its way</h2>
         <h3 className="text-primary text-center font-medium">Thank you for contacting with us</h3>
       </div>
       <img
-        className="mx-auto  min-w-[250px] max-w-[600px]"
+        className="mx-auto  min-h-[250px] h-auto  min-w-[250px] max-w-[600px]"
         src="/images/Rectangle 479.png"
         alt="order on its way"
       />
